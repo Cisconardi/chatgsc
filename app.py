ee3b6936461a39f065d54fff0c7054c41618a9a0
Francisco Nardi <116312660+Cisconardi@users.noreply.github.com>
2025-05-23 03:16:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -646,4 +646,4 @@ if st.session_state.get('show_privacy_policy', False):
         st.markdown(PRIVACY_POLICY_TEXT, unsafe_allow_html=False) 
     if st.button("Chiudi Informativa", key="close_privacy_policy_main_area"):
         st.session_state.show_privacy_policy = False
-        st.rerun() 
+        st.rerun()
